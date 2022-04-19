ee86f3ea8de480f542a52993e40cf4d67cf1c060
giahai99 <87426577+giahai99@users.noreply.github.com>
2022-04-19 18:14:31 +0700

    Create KanikoContainerTemplate.groovy

---
+++ resources/KanikoContainerTemplate.groovy
@@ -0,0 +1,13 @@
+def KanikoContainerTemplate {
+    return """
+kind: Pod
+spec:
+  containers:
+  - name: claranet
+    image: claranet/gcloud-kubectl-docker:latest
+    imagePullPolicy: Always
+    command:
+    - cat
+    tty: true
+"""
+}
