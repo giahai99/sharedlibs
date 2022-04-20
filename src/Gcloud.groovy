97f652adabf0484587f7dcc9263c3a5737cdf0d5
giahai99 <87426577+giahai99@users.noreply.github.com>
2022-04-20 15:09:03 +0700

    Update and rename GcloudAuthentication.groovy to Gcloud.groovy

--- src/Gcloud.groovy
+++ src/Gcloud.groovy
@@ -4,3 +4,7 @@ def authenticate(String key) {
     sh 'set +x; echo ${key} > key.json'
     sh 'gcloud auth activate-service-account truonggiahai-newaccount-primal@primal-catfish-346210.iam.gserviceaccount.com --key-file=key.json --project=primal-catfish-346210'
 }
+
+def getClusterCredentials() {
+    sh 'gcloud container clusters get-credentials cluster-1 --zone asia-southeast1-b --project primal-catfish-346210'    
+}
