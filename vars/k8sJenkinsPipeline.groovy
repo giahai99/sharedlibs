3e9fece1baf3e6a076ea87704a44bf3c60c6374b
giahai99 <87426577+giahai99@users.noreply.github.com>
2022-04-20 12:01:13 +0700

    Update k8sJenkinsPipeline.groovy

--- vars/k8sJenkinsPipeline.groovy
+++ vars/k8sJenkinsPipeline.groovy
@@ -20,7 +20,7 @@ def call() {
                   script{
                     withVault(configuration: [timeout: 60, vaultCredentialId: 'vault', vaultUrl: 'http://34.125.10.91:8200'], vaultSecrets: [[path: 'kv/service-account', secretValues: [[vaultKey: 'key']]]]) {
                         
-                        gcloud.authenticate("${key}")
+                      gcloud.authenticate(key)
                         
                         sh 'gcloud container clusters get-credentials cluster-1 --zone asia-southeast1-b --project primal-catfish-346210'
                     
