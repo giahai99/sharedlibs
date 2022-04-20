c3a49409b0857ea0e79e77907922bd4d4df44bb2
giahai99 <87426577+giahai99@users.noreply.github.com>
2022-04-20 15:34:22 +0700

    Update Kubectl.groovy

--- src/Kubectl.groovy
+++ src/Kubectl.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 
-def apply(def files) {
+def applyFiles(def files) {
   for(file in files) {
     sh "kubectl --namespace=devops-tools apply -f ${file}"
   }
@@ -8,5 +8,13 @@ def apply(def files) {
 
 
 def createDockerRegistrySecret(String password) {
-  sh 'kubectl create secret docker-registry docker-credentials --docker-username=giahai99 --docker-password=${password} --docker-email=Haidepzai_kut3@yahoo.com  --namespace=devops-tools'
+   sh 'kubectl create secret docker-registry docker-credentials --docker-username=giahai99 --docker-password=${password} --docker-email=Haidepzai_kut3@yahoo.com  --namespace=devops-tools'
+}
+
+def createGenericSecret(Map config = [:]){
+   sh "kubectl --namespace=devops-tools create secret generic db-user-pass --from-literal=username=${config.username} --from-literal=password=${config.password}"
+}
+
+def setDeploymentImage() {
+  sh 'kubectl --namespace=devops-tools set image deployment/book-deployment my-book-management=giahai99/javaapp:${BUILD_NUMBER}'  
 }
