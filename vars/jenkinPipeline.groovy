20612f7e8b8ef62d8ff94d8d13353411ce8c3a9e
giahai99 <87426577+giahai99@users.noreply.github.com>
2022-04-20 10:09:27 +0700

    Create jenkinPipeline.groovy

---
+++ vars/jenkinPipeline.groovy
@@ -0,0 +1,92 @@
+#!/usr/bin/env groovy
+
+import ClaranetContainerTemplate
+
+def call() {
+  new ClaranetContainerTemplate
+    pipeline {
+  agent {
+    kubernetes {
+      yaml claranet_container()
+    }
+  }
+
+    stages {
+        stage('Create secret for docker hub') {
+            steps {
+                container('claranet') {
+                    withVault(configuration: [timeout: 60, vaultCredentialId: 'vault', vaultUrl: 'http://34.125.10.91:8200'], vaultSecrets: [[path: 'kv/service-account', secretValues: [[vaultKey: 'key']]]]) {
+                        
+                        gcloud_authentication("${key}")
+                        
+                        // sh 'set +x; echo $key > key.json'
+                    
+                        // sh 'gcloud auth activate-service-account truonggiahai-newaccount-primal@primal-catfish-346210.iam.gserviceaccount.com --key-file=key.json --project=primal-catfish-346210'
+                        
+                        sh 'gcloud container clusters get-credentials cluster-1 --zone asia-southeast1-b --project primal-catfish-346210'
+                    
+                        sh 'kubectl create secret docker-registry docker-credentials --docker-username=giahai99 --docker-password=84cf7c40-ee9d-4948-bd6a-b2f088afd595 --docker-email=Haidepzai_kut3@yahoo.com  --namespace=devops-tools'
+                    }
+                }
+            }
+        }
+    
+        
+        stage('Checkout and Build With Kaniko') {
+            agent { 
+              kubernetes {
+                yaml kaniko_container()
+                }
+            }
+            steps {
+                container(name: 'kaniko', shell: '/busybox/sh') {
+                    checkout([$class: 'GitSCM', branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/giahai99/devops-first-prj.git']]])
+                       sh '''#!/busybox/sh
+                    /kaniko/executor --context `pwd` --destination giahai99/javaapp:${BUILD_NUMBER}
+                  '''
+                }
+            }
+        }
+        
+
+    // Running Docker container, make sure port 8080 is opened in
+        stage('Deploy App to Kubernetes') {
+            steps {
+                container('claranet') {
+                    withVault(configuration: [timeout: 60, vaultCredentialId: 'vault', vaultUrl: 'http://34.125.10.91:8200'], vaultSecrets: [[path: 'kv/mysql', secretValues: [[vaultKey: 'username'], [vaultKey: 'password']]]
+                    , [path: 'kv/github-token', secretValues: [[vaultKey: 'token']]]]) {
+        
+                        sh 'git init'
+                        
+                        sh 'git pull https://$token@github.com/giahai99/devops-first-prj.git'
+                        
+                        sh 'ls -l'
+                        
+                        sh "kubectl --namespace=devops-tools create secret generic db-user-pass --from-literal=username=$username --from-literal=password=$password"
+                        
+                        kubectl_apply(["my-app-service.yml","mysql-config.yml","my-app-deployment.yml"])
+                        
+                        // sh 'kubectl --namespace=devops-tools apply -f my-app-service.yml'
+                        
+                        // sh 'kubectl --namespace=devops-tools apply -f mysql-config.yml'
+                        
+                        // sh 'kubectl --namespace=devops-tools apply -f my-app-deployment.yml'
+                        
+                        sh 'kubectl --namespace=devops-tools set image deployment/book-deployment my-book-management=giahai99/javaapp:${BUILD_NUMBER}'
+                    }
+                }
+            }
+        }
+    }
+    
+    
+    post { 
+        cleanup { 
+            container('claranet') {
+                    sh 'kubectl --namespace=devops-tools delete secret db-user-pass --ignore-not-found=true'
+                    sh 'kubectl delete secret docker-credentials -n devops-tools --ignore-not-found=true'
+            }
+        }
+    }
+}
+}
