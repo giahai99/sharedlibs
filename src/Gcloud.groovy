f8c63ebdec12403203ce4a35645d6c60141dee55
giahai99 <87426577+giahai99@users.noreply.github.com>
2022-04-21 15:59:36 +0700

    test pipeline

--- src/Gcloud.groovy
+++ src/Gcloud.groovy
@@ -3,12 +3,8 @@
 
 def authenticate(String key, String serviceAccount, String project,String clusterName, String zone) {
     container('claranet') {
-        sh 'echo ${key}'
-        sh 'echo ${serviceAccount}'
-        sh 'echo serviceAccount'
-        sh 'echo ${clusterName} > hai.txt'
-        sh 'echo ${zone} > hai2.txt'
-        sh 'echo ${key} > key.json'
+        println ("$serviceAccount")
+        sh 'set +x ;echo ${key} > key.json'
         sh 'gcloud auth activate-service-account ${serviceAccount} --key-file=key.json --project=${project}'
         sh 'gcloud container clusters get-credentials ${clusterName} --zone asia-southeast1-b --project primal-catfish-346210'
     }
