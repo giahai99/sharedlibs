800edfbe9f8f7f940757776163bd14c01de7d606
giahai99 <87426577+giahai99@users.noreply.github.com>
2022-04-21 16:48:35 +0700

    test pipeline

--- src/Gcloud.groovy
+++ src/Gcloud.groovy
@@ -3,8 +3,9 @@
 
 def authenticate(Map config = [:]) {
     container('claranet') {
-        println("hello1")
-        sh "set +x ;echo $config.key > key.json"
+        println("$config.key")
+        println('$config.key')
+        sh 'set +x ;echo $config.key > key.json'
         println("hello2")
         sh "gcloud auth activate-service-account $config.serviceAccount --key-file=key.json --project=$config.project"
         println("hello3")
