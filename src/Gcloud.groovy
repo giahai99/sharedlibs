b13fc06178da7bb6c88cef098407019565549ff0
giahai99 <87426577+giahai99@users.noreply.github.com>
2022-04-21 14:14:06 +0700

    test pipeline

--- src/Gcloud.groovy
+++ src/Gcloud.groovy
@@ -1,14 +1,12 @@
 #!/usr/bin/env groovy
 
-class Gcloud {
 
-    def authenticate(String key, String serviceAccount, String project) {
-        sh 'set +x; echo ${key} > key.json'
-        sh 'gcloud auth activate-service-account ${serviceAccount} --key-file=key.json --project=${project}'
-    }
-
-    def getClusterCredentials(Map config = [:]) {
-        sh 'gcloud container clusters get-credentials ${config.clusterName} --zone ${config.zone} --project ${config.project}'
-    }
-
+def authenticate(Map config = [:]) {
+    sh 'set +x; echo ${config.key} > key.json'
+    sh 'gcloud auth activate-service-account ${config.serviceAccount} --key-file=key.json --project=${config.project}'
 }
+
+def getClusterCredentials(Map config = [:]) {
+    sh 'gcloud container clusters get-credentials ${config.clusterName} --zone ${config.zone} --project ${config.project}'
+}
+
--- src/Kubectl.groovy
+++ src/Kubectl.groovy
@@ -1,41 +1,40 @@
 #!/usr/bin/env groovy
-class Kubectl {
 
-    def applyFiles(Map config= [:]) {
-        container('claranet') {
-            for (file in config.fileList) {
+def applyFiles(Map config= [:]) {
+    container('claranet') {
+        for (file in config.fileList) {
 
-                sh "cd ./$config.directory && kubectl --namespace=$config.nameSpace apply -f ${file}"
+            sh "cd ./$config.directory && kubectl --namespace=$config.nameSpace apply -f ${file}"
 
-            }
-        }
-    }
-
-    def createDockerRegistrySecret(Map config = [:]) {
-        container('claranet') {
-            sh 'kubectl create secret docker-registry docker-credentials --docker-username=${config.username} --docker-password=${config.password} --docker-email=${config.dockerEmail}  --namespace=${config.nameSpace}'
-        }
-    }
-
-    def createGenericSecret(Map config = [:]) {
-        container('claranet') {
-            sh "kubectl --namespace=devops-tools create secret generic $config.secretName --from-literal=username=${config.username} --from-literal=password=${config.password}"
-        }
-    }
-
-    def setDeploymentImage(Map config = [:]) {
-        container('claranet') {
-            sh 'kubectl --namespace=$config.nameSpace set image deployment/$config.deploymentName $config.containerName=$config.dockerImage:$config.tag'
-        }
-    }
-
-    def deleteSecretAfterRun(Map config = [:]) {
-        container('claranet') {
-            for (secret in config.secrets) {
-
-                sh 'kubectl --namespace=$config.nameSpace delete secret $secret --ignore-not-found=true'
-
-            }
         }
     }
 }
+
+def createDockerRegistrySecret(Map config = [:]) {
+    container('claranet') {
+        sh 'kubectl create secret docker-registry docker-credentials --docker-username=${config.username} --docker-password=${config.password} --docker-email=${config.dockerEmail}  --namespace=${config.nameSpace}'
+    }
+}
+
+def createGenericSecret(Map config = [:]) {
+    container('claranet') {
+        sh "kubectl --namespace=devops-tools create secret generic $config.secretName --from-literal=username=${config.username} --from-literal=password=${config.password}"
+    }
+}
+
+def setDeploymentImage(Map config = [:]) {
+    container('claranet') {
+        sh 'kubectl --namespace=$config.nameSpace set image deployment/$config.deploymentName $config.containerName=$config.dockerImage:$config.tag'
+    }
+}
+
+def deleteSecretAfterRun(Map config = [:]) {
+    container('claranet') {
+        for (secret in config.secrets) {
+
+            sh 'kubectl --namespace=$config.nameSpace delete secret $secret --ignore-not-found=true'
+
+        }
+    }
+}
+
