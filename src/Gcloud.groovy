c7bec2a8bb309825a3bb839e4a9f536c45560d3c
giahai99 <87426577+giahai99@users.noreply.github.com>
2022-04-21 15:47:59 +0700

    test pipeline

--- src/Gcloud.groovy
+++ src/Gcloud.groovy
@@ -1,17 +1,19 @@
 #!/usr/bin/env groovy
 
 
-def authenticate(String key, String serviceAccount, String project) {
+def authenticate(String key, String serviceAccount, String project,String clusterName, String zone) {
     container('claranet') {
+        sh 'echo ${clusterName} > hai.txt'
+        sh 'echo ${zone} > hai2.txt'
         sh 'echo ${key} > key.json'
         sh 'gcloud auth activate-service-account ${serviceAccount} --key-file=key.json --project=${project}'
     }
 }
 
-def getClusterCredentials(String serviceAccount, String zone, String project) {
+def getClusterCredentials(String clusterName, String zone, String project) {
     container('claranet') {
-        sh 'echo ${serviceAccount} > hai.txt'
+        sh 'echo ${clusterName} > hai.txt'
         sh 'echo ${zone} > hai.txt'
-        sh 'gcloud container clusters get-credentials ${serviceAccount} --zone asia-southeast1-b --project primal-catfish-346210'
+        sh 'gcloud container clusters get-credentials ${clusterName} --zone asia-southeast1-b --project primal-catfish-346210'
     }
 }
--- vars/k8sJenkinsPipeline.groovy
+++ vars/k8sJenkinsPipeline.groovy
@@ -21,7 +21,7 @@ def call() {
                             withVault(configuration: [timeout: 60, vaultCredentialId: 'vault', vaultUrl: 'http://34.125.10.91:8200'], vaultSecrets: [[path: 'kv/service-account', secretValues: [[vaultKey: 'key']]],
                                                                                                                                                      [path: 'kv/dockerhub-password', secretValues: [[vaultKey: 'password']]]]) {
 
-                                gcloud.authenticate(key,"truonggiahai-newaccount-primal@primal-catfish-346210.iam.gserviceaccount.com","primal-catfish-346210")
+                                gcloud.authenticate(key,"truonggiahai-newaccount-primal@primal-catfish-346210.iam.gserviceaccount.com","primal-catfish-346210","cluster-1","asia-southeast1-b")
 
                                 gcloud.getClusterCredentials("cluster-1","asia-southeast1-b","primal-catfish-346210")
 
