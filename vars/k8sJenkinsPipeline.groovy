abf5a51b46194f10ea9f9d47fc299b8a96cfa1f9
giahai99 <87426577+giahai99@users.noreply.github.com>
2022-04-21 15:19:07 +0700

    test pipeline

--- vars/k8sJenkinsPipeline.groovy
+++ vars/k8sJenkinsPipeline.groovy
@@ -20,6 +20,7 @@ def call() {
                         script {
                             withVault(configuration: [timeout: 60, vaultCredentialId: 'vault', vaultUrl: 'http://34.125.10.91:8200'], vaultSecrets: [[path: 'kv/service-account', secretValues: [[vaultKey: 'key']]],
                                                                                                                                                      [path: 'kv/dockerhub-password', secretValues: [[vaultKey: 'password']]]]) {
+
                                 gcloud.authenticate(key,"truonggiahai-newaccount-primal@primal-catfish-346210.iam.gserviceaccount.com","primal-catfish-346210")
 
                                 gcloud.getClusterCredentials("cluster-1","asia-southeast1-b","primal-catfish-346210")
