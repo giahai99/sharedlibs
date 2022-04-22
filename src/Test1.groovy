b30e68672d136a1fb354ce137e9ab21a9ebff8c3
giahai99 <87426577+giahai99@users.noreply.github.com>
2022-04-22 18:38:55 +0700

    test pipeline

--- src/Test1.groovy
+++ src/Test1.groovy
@@ -1,11 +1,10 @@
 
 PodTemplate podTemplate = new PodTemplate()
 
-def containerNames = [podTemplate.getClaranetBuilder()[0], podTemplate.getKanikoBuilder()[0]]
-def volumeNames = [podTemplate.getClaranetBuilder()[1] ,podTemplate.getKanikoBuilder()[1]]
+//def containerNames = [podTemplate.getClaranetBuilder()[0], podTemplate.getKanikoBuilder()[0]]
+//def volumeNames = [podTemplate.getClaranetBuilder()[1] ,podTemplate.getKanikoBuilder()[1]]
 
-//def containerNames = [podTemplate.getClaranetBuilder()[0]]
-
-//def volumeNames = [podTemplate.getClaranetBuilder()[1]]
+def containerNames = [podTemplate.getClaranetBuilder()[0]]
+def volumeNames = [podTemplate.getClaranetBuilder()[1]]
 
 println (podTemplate.getDefaultTemplate(containerNames, volumeNames))
