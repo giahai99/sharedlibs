8ae70ea794198038b21e59fb6a89e85f78067826
giahai99 <87426577+giahai99@users.noreply.github.com>
2022-04-22 15:03:56 +0700

    test pipeline

--- vars/k8sJenkinsPipeline.groovy
+++ vars/k8sJenkinsPipeline.groovy
@@ -4,27 +4,88 @@ def call() {
     PodTemplate podTemplate = new PodTemplate()
     StageOperator stageOperator = new StageOperator()
 
-    kubernetes {
-        yaml podTemplate.addKanikoBuilder()
-    }
 
-    podTemplate {
-        node(POD_LABEL) {
+
+//    podTemplate {
+//        kubernetes {
+//            yaml podTemplate.addKanikoBuilder()
+//        }
+//        node(POD_LABEL) {
+//            stage('Create secret for docker hub') {
+//
+//
+//                withVault(configuration: [timeout: 60, vaultCredentialId: 'vault', vaultUrl: 'http://34.125.10.91:8200'], vaultSecrets: [[path: 'kv/service-account', secretValues: [[vaultKey: 'key']]],
+//                                                                                                                                         [path: 'kv/dockerhub-password', secretValues: [[vaultKey: 'password']]]]) {
+//
+//                    stageOperator.createDockerHubSecret(serviceAccountKey: key, clusterName: "cluster-1", username: "giahai99", password: password, namespace: "devops-tools")
+//                }
+//
+//
+//            }
+//        }
+//    }
+
+    pipeline {
+        agent {
+            kubernetes {
+                yaml podTemplate.addClaranetBuilder()
+            }
+        }
+
+        stages {
             stage('Create secret for docker hub') {
+                steps {
+                    script {
+                        withVault(configuration: [timeout: 60, vaultCredentialId: 'vault', vaultUrl: 'http://34.125.10.91:8200'], vaultSecrets: [[path: 'kv/service-account', secretValues: [[vaultKey: 'key']]],
+                                                                                                                                                 [path: 'kv/dockerhub-password', secretValues: [[vaultKey: 'password']]]]) {
 
-
-                withVault(configuration: [timeout: 60, vaultCredentialId: 'vault', vaultUrl: 'http://34.125.10.91:8200'], vaultSecrets: [[path: 'kv/service-account', secretValues: [[vaultKey: 'key']]],
-                                                                                                                                         [path: 'kv/dockerhub-password', secretValues: [[vaultKey: 'password']]]]) {
-
-                    stageOperator.createDockerHubSecret(serviceAccountKey: key, clusterName: "cluster-1", username: "giahai99", password: password, namespace: "devops-tools")
+                            stageOperator.createDockerHubSecret(serviceAccountKey: key,clusterName: "cluster-1", username: "giahai99", password: password, namespace: "devops-tools")
+                        }
+                    }
                 }
+            }
 
 
+            stage('Checkout and Build With Kaniko') {
+                agent {
+                    kubernetes {
+                        yaml podTemplate.addKanikoBuilder()
+                    }
+                }
+                steps {
+                    script{
+                        stageOperator.checkoutBuildAndPushImage(branch: "main", url: "https://github.com/giahai99/devops-first-prj.git", dockerImage: "giahai99/javaapp")
+                    }
+                }
+            }
+
+
+            // Running Docker container, make sure port 8080 is opened in
+            stage('Deploy App to Kubernetes') {
+                steps {
+                    script{
+                        withVault(configuration: [timeout: 60, vaultCredentialId: 'vault', vaultUrl: 'http://34.125.10.91:8200'], vaultSecrets: [[path: 'kv/mysql', secretValues: [[vaultKey: 'username'], [vaultKey: 'password']]]
+                                                                                                                                                 , [path: 'kv/github-token', secretValues: [[vaultKey: 'token']]]]) {
+
+                            stageOperator.deployAppToKubernetes(organization: "giahai99", resporitory: "devops-first-prj.git", secretName: "db-user-pass",
+                                    secrets: [username: username, password: password], namespace: "devops-tools")
+                        }
+                    }
+                }
+            }
+        }
+
+
+        post {
+            cleanup {
+                script{
+
+                    stageOperator.deleteSecretAfterRun(namespace: "devops-tools", secrets: ["db-user-pass","docker-credentials"])
+
+                }
             }
         }
     }
-
-
 
 
 //        node(
