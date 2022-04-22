491cdf0136ce1c073db4c8ede2df9268ee744b55
giahai99 <87426577+giahai99@users.noreply.github.com>
2022-04-22 17:33:37 +0700

    test pipeline

--- vars/k8sJenkinsPipeline.groovy
+++ vars/k8sJenkinsPipeline.groovy
@@ -4,7 +4,12 @@ def call() {
     PodTemplate podTemp = new PodTemplate()
     StageOperator stageOperator = new StageOperator()
 
-    podTemplate(yaml: podTemp.addClaranetBuilder()) {
+    def containerNames = [podTemplate.getClaranetBuilder()[0], podTemplate.getKanikoBuilder()[0]]
+    def volumeNames = [podTemplate.getClaranetBuilder()[1] ,podTemplate.getKanikoBuilder()[1]]
+
+
+
+    podTemplate(yaml: podTemp.getDefaultTemplate(containerNames, volumeNames)) {
         node(POD_LABEL) {
             stage('Create secret for docker hub') {
                 withVault(configuration: [timeout: 60, vaultCredentialId: 'vault', vaultUrl: 'http://34.125.10.91:8200'], vaultSecrets: [[path: 'kv/service-account', secretValues: [[vaultKey: 'key']]],
@@ -16,7 +21,7 @@ def call() {
         }
     }
 
-    podTemplate(yaml: podTemp.addKanikoBuilder()) {
+    podTemplate(yaml: podTemp.getDefaultTemplate(containerNames, volumeNames)) {
         node(POD_LABEL) {
             stage('Checkout and Build With Kaniko') {
                 stageOperator.checkoutBuildAndPushImage(branch: "main", url: "https://github.com/giahai99/devops-first-prj.git", dockerImage: "giahai99/javaapp")
@@ -24,7 +29,7 @@ def call() {
             }
         }
 
-    podTemplate(yaml: podTemp.addClaranetBuilder()) {
+    podTemplate(yaml: podTemp.getDefaultTemplate(containerNames, volumeNames)) {
         node(POD_LABEL) {
             stage('Create secret for docker hub') {
                 withVault(configuration: [timeout: 60, vaultCredentialId: 'vault', vaultUrl: 'http://34.125.10.91:8200'], vaultSecrets: [[path: 'kv/service-account', secretValues: [[vaultKey: 'key']]],
@@ -35,7 +40,7 @@ def call() {
         }
     }
 
-        podTemplate(yaml: podTemp.addClaranetBuilder()) {
+        podTemplate(yaml: podTemp.getDefaultTemplate(containerNames, volumeNames)) {
             node() {
             stage('Clean up after run') {
                 stageOperator.deleteSecretAfterRun(namespace: "devops-tools", secrets: ["db-user-pass", "docker-credentials"])
