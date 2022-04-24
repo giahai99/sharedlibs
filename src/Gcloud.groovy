546687e149d61f4a1cf86291d0fb9964600389bc
Truong Gia Hai <Haidepzai_kut3@yahoo.com>
2022-04-24 14:30:29 +0700

    updated k8sjenkinsfile

--- src/Gcloud.groovy
+++ src/Gcloud.groovy
@@ -4,11 +4,11 @@ def authenticate(Map config = [:]) {
     container('claranet') {
         String key = config.key
 //        println(key)
-//        writeFile(file: 'key.json', text: key)
+        writeFile(file: "key.json", text: key)
 //        sh 'cat key.json'
 //        sh 'set +x ;echo $key > key.json'
-        sh 'echo $key'
-        sh 'set +x; echo $key > key.json'
+//        sh 'echo $key'
+//        sh 'set +x; echo $key > key.json'
         sh "gcloud auth activate-service-account $config.serviceAccount --key-file=key.json --project=$config.project"
     }
 }
